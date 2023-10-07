afc01e15f20fa301215c2c2037601cb7d44ff432
David Castellanos <davcastellanoslarios@gmail.com>
2023-10-07 14:16:56 -0600

    working nav bar

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,23 +1,37 @@
-import { useState } from "react"
+import { useState, useRef } from "react"
 import { FaBars } from 'react-icons/fa'
 import { links, social } from './data'
 import logo from './logo.svg'
 
 export const Navbar = () => {
 
-    const [showLinks, setShowLinks] = useState(false)
+    const [showLinks, setShowLinks] = useState(false);
+    const linksContainerRef = useRef(null)
+    const linksRef = useRef(null)
+
+    const toggleLinks = () => {
+        
+        setShowLinks(!showLinks)
+    }
+
+    const linkStyles = {
+        height: showLinks 
+        ? `${linksRef.current.getBoundingClientRect().height}px` 
+        : '0px',
+    }
 
     return (
     <nav>
         <div className="nav-center">
           <div className="nav-header">
           <img src={logo} alt="logo" className="logo" />
-          <button className="nav-toggle">
+          <button className="nav-toggle" onClick={toggleLinks}>
             <FaBars />
           </button>
           </div>
-          <div className="links-container">
-            <ul className="links">
+          
+          <div className='links-container' ref={linksContainerRef} style={linkStyles}>
+            <ul className="links" ref={linksRef}>
                 {
                     links.map(link => {
                         const {id, url, text} = link;
@@ -28,6 +42,7 @@ export const Navbar = () => {
                 }
             </ul>
           </div>
+          
         </div>
     </nav>
   )
